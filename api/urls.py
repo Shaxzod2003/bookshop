b525150947ea52b24d3e3f11bb5708b3e6927b8b
Shaxzod2003 <sulaymonovshaxzod77@>
2023-11-09 09:55:14 +0500

    views

--- api/urls.py
+++ api/urls.py
@@ -1,1 +1,9 @@
 from django.urls import path
+from . import views
+
+urlpatterns=[
+    path("customers/", views.CustomersView.as_view()),
+    path("customer/<int:pk>/", views.CustomerDetailsView.as_view()),
+    path("contacts/", views.ContactsView.as_view()),
+    path("contact/<int:pk>/",views.ContactsDetailsView.as_view())
+]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.views import View
+from django.http import HttpRequest,JsonResponse
+from .models import Customer,Contact
+from django.forms import model_to_dict
+import json
 
-# Create your views here.
+class CustomersView(View):
+    def get(self,request:HttpRequest) ->JsonResponse:
+        results=[]
+        for customer in Customer.objects.all():
+            results.append(model_to_dict(customer, fields=["id","first_name", "last_name", "username"]))
+        return JsonResponse(results, safe=False)
+    
+    def post(self,request: HttpRequest)->JsonResponse:
+        body=request.body.decode()
+        data=json.loads(body)
+        Customer.objects.create(
+            first_name=data.get("first_name"),
+            last_name=data.get("last_name"),
+            username=data.get("username"),
+            password=data.get("password")
+        )
+        return {"message": "created objects"}
+
+
+class CustomerDetailsView(View):
+    pass
+
+class ContactsView(View):
+    pass
+
+
+class ContactsDetailsView(View):
+    pass
